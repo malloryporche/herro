67df7c14c7196baf1f5398f6777021813a897ddf
Mallory Porche <malloryburke@gmail.com>
2015-07-27 14:32:57 -0700

    Added Favorites star to board Page

--- client/templates/boards/boardPage.js
+++ client/templates/boards/boardPage.js
@@ -3,3 +3,27 @@ Template.boardPage.helpers({
 		return Tasklists.find();
 	}
 })
+Template.boardPage.events({
+    'click .favorites': function( e, t ){
+        // code goes here
+        // debugger
+
+        var _id = this._id,
+        	checked = $(e.currentTarget).is(':checked');
+        // set this._id to favorites true in collection
+        // debugger
+        if ( checked ) {
+        	Meteor.call('updateFavorites', _id, true, function( error, result) { 
+             if (error) {
+               throw new Meteor.Error(error);
+             }
+            });
+    	} else {
+    		Meteor.call('updateFavorites', _id, false, function( error, result) { 
+             if (error) {
+               throw new Meteor.Error(error);
+             }
+            });
+    	}
+    }
+});
