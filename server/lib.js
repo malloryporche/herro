d2dd2b192c159b34ecba7b6f1247e898a89f7d49
MalloryPorche <malloryburke@gmail.com>
2015-08-02 16:43:19 -0700

    Upload to dsktp

--- server/lib.js
+++ server/lib.js
@@ -10,6 +10,8 @@ addNewBoards: function (value0, value1, value2) {
  },
 deleteBoards: function (value0) {
      return  Boards.remove({ '_id': value0});
+     _alerts("success", "Board successfully deleted");
+
  },
 addNewTaskList: function (value0, value1, value2) {
     
@@ -17,6 +19,7 @@ addNewTaskList: function (value0, value1, value2) {
     var titleExists = TaskLists.findOne({'taskListTitle': value0});
    
    if (titleExists) {
+        //Prevent user from inputting a duplicate tasklist title
         _alerts("error", "You already have a Tasklist with this title on this board.  Please enter a new tasklist name.");}
     else {
      var addNewTaskLists = TaskLists.insert({ 
@@ -26,10 +29,12 @@ addNewTaskList: function (value0, value1, value2) {
      });
      console.log("Code executed");
    return addNewTaskLists;
+     _alerts("success", "Tasklist successfully created");
    };
 },
 deleteTaskList: function (value0) {
      return TaskLists.remove({ '_id': value0});
+       _alerts("success", "Tasklist successfully deleted");
  },
 addNewCards: function(value0, value1, value2, value3, value4, value5) {
    var addNewCard = Cards.insert({ 
@@ -41,15 +46,18 @@ addNewCards: function(value0, value1, value2, value3, value4, value5) {
          'taskTitle' : value5
      });
    return addNewCard;
+     _alerts("success", "Card successfully created");
  },
 
 deleteCards: function (value0) {
      return Cards.remove({ '_id': value0});
+       _alerts("success", "Card successfully deleted");
  },
 updateFavorites: function(value0, value1) {
     var updateFavorites = Boards.update({'_id' : value0,}, 
                                         {$set: {favorite: value1}});
         return updateFavorites;
+          _alerts("success", "Board successfully updated");
     }
 
 });
