f65b4e8f8bbb2125147e843b56ae3851de64e6b9
Mallory Porche <malloryburke@gmail.com>
2015-08-01 21:11:53 -0700

    Refactored header

--- client/app/includes/header.js
+++ client/app/includes/header.js
@@ -1,30 +1,17 @@
-Template.AddNewBoard.events({
-	//When form is submitted via Enter
-	'submit input.newBoard': function(e, t) {
-		 	//Prevent default submission of form
-			e.preventDefault();
+Template.Header.events({
+	
+	// Activate Side Navigation for Mobile
+	'click a.button-collapse': function(e,t) {
+   		$('.button-collapse').sideNav({
+		     menuWidth: 300, // Default is 240
+		     edge: 'left', // Choose the horizontal origin
+		     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+   }
+ );
+   	},
 
-		//Get access to form itself
-		var formEl = t.find('form'),
-			boardTitleElement = t.find('[name=addNewBoard]'),
-			boardTitle = boardTitleElement.value,
-			title = boardTitle,
-			timestamp = new Date,
-			boardId = this._id;
-
-		//Method call to create a new Board and add it to collection
-		Meteor.call('addNewBoards', title, timestamp, boardId, function( error, result) { 
-             if (error) {
-               throw new Meteor.Error("Problem creating new board");
-
-             }
-
-		//Clear form
-		formEl.reset();
-		});
-
-	},
-
-	'click a.button-collapse':  function(){
-   $(".button-collapse").sideNav();	}
+   //Toggle between ACTIVE NAVBAR and Search form
+   'click a.search': function() {
+   		return Session.set('isSearching', true)
+   }
 });
